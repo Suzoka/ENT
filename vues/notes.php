6899ec238e19147183996356a03069b79520f7bf
Suzoka/Morgan <mor9an77@gmail.com>
2024-01-05 12:40:15 +0100

    :bug: when modules are in several skills

--- vues/notes.php
+++ vues/notes.php
@@ -30,17 +30,19 @@
                     <div class="notes">
                         <?php
                         foreach (getAllModsOfComp($comp["id_competence"]) as $mod) { ?>
-                            <div class="title"><button class="developMod" id="<?php echo $mod["id_module"] ?>">
+                            <div class="title"><button class="developMod" id="<?php echo $comp["id_competence"].$mod["id_module"] ?>">
                                     <h3><?php echo $mod["nom_module"] ?></h3>
                                 </button>
                                 <p class="moyenneMod">
                                 <?php echo formatNote(getMoyenneMod($mod["id_module"], $_SESSION["login"])); ?>
                                 </p>
                             </div>
-                            <div class="devoirs data<?php echo $mod["id_module"] ?>">
+                            <div class="devoirs data<?php echo $comp["id_competence"].$mod["id_module"] ?>">
                                 <?php 
                                 foreach (getAllDevoirsOfMod($mod["id_module"], $_SESSION["login"])->fetchAll(PDO::FETCH_ASSOC) as $devoir) { ?>
-                                <div class="title noteDevoir"><h4><?php echo $devoir["nom_devoir"]; ?></h4><p><?php echo formatNote($devoir["valeur"]); ?></p></div>
+                                <div class="title noteDevoir"><div class="devoirInfos">
+                                    <h4><?php echo $devoir["nom_devoir"]; ?></h4><p class="coef">coefficient <?php echo $devoir["coef_devoir"]; ?></p>
+                                </div><p><?php echo formatNote($devoir["valeur"]); ?></p></div>
                                 <?php } ?>
                             </div>
                         <?php } ?>
