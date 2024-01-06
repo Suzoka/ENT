6c97a29358171791bb3445545c2eb025f1f36acc
wally <waldifiaga8@gmail.com>
2024-01-06 18:04:10 +0100

    💄front sans font
    j'ai fais le font il manque les font style

--- vues/profil.php
+++ vues/profil.php
@@ -13,7 +13,36 @@
     <?php require './vues/components/header.php'; ?>
     <section class="grid">
         <div class="profil customScroll">
-
+            <div class="character">
+                <img src="<?php echo $image ?>" alt="">
+                <div class="youu">
+                    <div class="text">
+                        <h1><?php echo $identite ?></h1>
+                        <p><?php echo "num etud" ?></p>
+                    </div>
+                    <p><?php echo $classes; ?></p>
+                    <p><?php echo "statut quand je saurrais comment l'appeller mdrrr" ?></p>
+                </div>
+            </div>
+            <a class="bouton" href="#">telecharger mes certificat de scolaritée</a>
+            <div class="contenu">
+                <div class="zone moi">
+                    <h2>A propos de moi</h2>
+                    <p><?php echo "texte a recupe avec php j'imagine" ?></p>
+                    <a class="bouton" href="#">telecharger mon Cv</a>
+                </div>
+                <div class="zone projets ">
+                    <h2>mes projets</h2>
+                    <div class="projets-list customScroll">
+                        <div class="projet"></div>
+                        <div class="projet"></div>
+                        <div class="projet"></div>
+                        <div class="projet"></div>
+                        <div class="projet"></div>
+                        <!-- <?php echo "" ?> -->
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </body>
